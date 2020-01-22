2a1d5ea37619d0aa94c00275c5510e4444c27d7a
Chris Colborne <zorfling@gmail.com>
2020-01-23 09:01:42 +1000

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -71,6 +71,32 @@ const Template = ({ location, children }) => {
           name="google-site-verification"
           content="cFwycapKTaXQNg_YXy4GuPrN2NXWfU45dcS-AN7xbF8"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#bec3d2" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Modern JS for PHP Devs"
+        />
+        <meta name="application-name" content="Modern JS for PHP Devs" />
+        <meta name="msapplication-TileColor" content="#bec3d2" />
+        <meta name="theme-color" content="#bec3d2"></meta>
       </Helmet>
       {header}
       {children()}
